def39d5c5b3ca9985aa0083f058c8d70baaa8d43
James Cori <jamescori@gmail.com>
2020-09-22 18:56:00 -0400

    Trying updated build script

--- build.sh
+++ build.sh
@@ -9,7 +9,7 @@ docker create --name app $APP_NAME:latest
 if [ -d node_modules ]
 then
   mv package-lock.json old-package-lock.json
-  docker cp app:/legacy-challenge-resource-processor/package-lock.json package-lock.json
+  docker cp app:/app/package-lock.json package-lock.json
   set +eo pipefail
   UPDATE_CACHE=$(cmp package-lock.json old-package-lock.json)
   set -eo pipefail
@@ -19,5 +19,5 @@ fi
 
 if [ "$UPDATE_CACHE" == 1 ]
 then
-  docker cp app:/legacy-challenge-resource-processor/node_modules .
+  docker cp app:/app/node_modules .
 fi
